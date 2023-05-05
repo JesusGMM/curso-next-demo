5b352ca48b26e9096b028dec22d175be44f8ffc8
Jesus M <jesus.g.m30@hotmail.com>
2023-05-04 22:20:46 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -107,6 +108,17 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
+
+          <Link
+            href="/about"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Ir about <span>-&gt;</span>
+            </h2>
+          </Link>
+
         </div>
       </main>
     </>
